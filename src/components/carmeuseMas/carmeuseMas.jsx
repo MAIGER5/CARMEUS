ff697e8657f8d6c644bafaca37567b623d569e9d
maiger5 <maiger5learning@gmail.com>
2024-08-13 22:58:53 -0500

    nueva actualizacion de carmeuse mas

--- src/components/carmeuseMas/carmeuseMas.jsx
+++ src/components/carmeuseMas/carmeuseMas.jsx
@@ -5,6 +5,9 @@ import DataContext from '../dataContext/dataContext';
 import { ScreenForm2 } from '../screens/screenForm2';
 import { BsFillPlusSquareFill } from 'react-icons/bs';
 import dataCarmeuseMas from '../utils/data/dataCarmeuseMas';
+import imagenis from '../utils/icon/fer.jpeg'
+import imagenis2 from '../utils/icon/ferreter.jpg'
+import { FormPay } from './formPay/formPay';
 
 
 export const CarmeuseMas = () => {
@@ -49,43 +52,48 @@ export const CarmeuseMas = () => {
 
 
       <div className={styles.secondColumn}>
-        <form action="" className={styles.formPagos}>
-          <h1>Pagos PayU</h1>
-          <div className={styles.nameApellido}>
-            <input 
-              type="text"
-              name='nombre'
-              placeholder='Nombres'
-              required
-            />
-            <input 
-              type="text"
-              name='apellido'
-              placeholder='Apellidos'
-              required
-            />
+        {/* <div className={styles.ferretero}>
+          <div className={styles.screenFerretero}></div>
+          <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={imagenis} className="d-block w-100" alt="..."/>
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>First slide label</h5>
+                  <p>Some representative placeholder content for the first slide.</p>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img src={imagenis2} className="d-block w-100" alt="..."/>
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>Second slide label</h5>
+                  <p>Some representative placeholder content for the second slide.</p>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img src={imagenis} className="d-block w-100" alt="..."/>
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>Third slide label</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
           </div>
-          <input 
-            type="number"
-            name='nit'
-            placeholder='Nit o Cedula'
-            required
-            color='#fafafa'
-          />
-          <input 
-            type="email"
-            name='mail'
-            placeholder='Correo electrónico'
-            required
-          />
-          <input 
-            type="number"
-            name='monto'
-            placeholder='Monto'
-            required
-          />
-          <button>Pagar</button>
-        </form>
+        </div> */}
+        <FormPay/>
       </div>
       
     </div>
